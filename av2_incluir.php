6a4bd78c190e86f46c0c393072d3cc7404b66aad
Sérgio Raphael A. Sampaio <67436514+Svetrar@users.noreply.github.com>
2021-12-16 10:31:20 -0300

    Add files via upload

---
+++ av2_incluir.php
@@ -0,0 +1,34 @@
+<?php
+
+    $nome = $_GET["nome"];
+	$endereco = $_GET["endereco"];
+	$postalCode = $_GET["nome"];
+	$pais = $_GET["pais"];
+	$cpf = $_GET[" cpf"];
+	$passaporte = $_GET["passaporte"];
+	$email = $_GET["email"];
+	$dataNascimento = $_GET["dataNascimento"];
+
+    $servidor = "localhost";
+    $username = "root";
+    $senha = "";
+    $database = "av2_banco";
+    $conn = new mysqli($servidor,$username,$senha, $database);
+	
+    if ($conn->	connect_error) 
+	{
+        die("Erro de conexão");
+    }
+	
+	$comandoSQL = "INSERT INTO `av2_clientes`(`nome`, `endereco`, `postalCode`, `pais`, `cpf`, 
+					`passaporte`, `email`, `dataNascimento`) VALUES ('$nome',$endereco,'$postalCode','$pais'
+					,'$cpf','$passaporte','$email','$dataNascimento')";
+    $result = $conn->query($comandoSQL);
+	
+	$comandoSQL = "SELECT `nome`, `endereco`, `postalCode`, `pais`, `cpf`, `passaporte`, 
+					`email`, `dataNascimento` FROM `av2_clientes` WHERE nome = '$nome'";
+    $result = $conn->query($comandoSQL);
+    $clienteJSON = json_encode($result->fetch_assoc(), JSON_UNESCAPED_UNICODE);
+
+    echo $clienteJSON;
+?>
